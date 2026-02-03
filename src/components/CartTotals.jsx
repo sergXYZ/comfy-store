f9a50b8023414e16c7ccc499d912f3fc6a0c5506
sergXYZ <serhiis1993@gmail.com>
2026-02-03 20:36:34 +0100

    add part of  cart section

---
+++ src/components/CartTotals.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { formatPrice } from '../utils'
+
+const CartTotals = () => {
+  const { cartTotal, shipping, tax, orderTotal } = useSelector(
+    (state) => state.cartState,
+  )
+  return (
+    <div className="card bg-base-200">
+      <div className="card-body">
+        {/* {SUBTOTAL} */}
+        <p className="flex justify-between text-xs border-b border-base-300 pb-2">
+          <span>Subtotal</span>
+          <span>{formatPrice(cartTotal)}</span>
+        </p>
+        {/* {SHIPPING} */}
+        <p className="flex justify-between text-xs border-b border-base-300 pb-2">
+          <span>Shipping</span>
+          <span>{formatPrice(shipping)}</span>
+        </p>
+        {/* {TAX} */}
+        <p className="flex justify-between text-xs border-b border-base-300 pb-2">
+          <span>Tax</span>
+          <span>{formatPrice(tax)}</span>
+        </p>
+        {/* {ORDER TOTAL} */}
+        <p className="flex justify-between text-sm mt-4 pb-2">
+          <span>Total Order</span>
+          <span>{formatPrice(orderTotal)}</span>
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default CartTotals
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,7 +1,30 @@
 import React from 'react'
 
+import { SectionTitle, CartTotals, CartItemsList } from '../components'
+import { useSelector } from 'react-redux'
+
 const Cart = () => {
-  return <div>Cart</div>
+  const user = null
+  const numberItemsInCart = useSelector(
+    (store) => store.cartState.numItemsInCart,
+  )
+  if (numberItemsInCart === 0) {
+    return <SectionTitle text={'your shopping cart is epmty'} />
+  }
+
+  return (
+    <div>
+      <SectionTitle text={'shoping cart'} />
+      <div className="mt-8 grid gap-8 lg:grid-cols-12 ">
+        <div className="lg:col-span-8">
+          <CartItemsList className="lg:col-span-8" />
+        </div>
+        <div className="lg:col-span-4 pl-4">
+          <CartTotals />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Cart
